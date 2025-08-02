d0766d29d12bb565dc65e8dcf4e402f5e3bb8246
Marcos Vieira <marcos.vieira.nbt@outlook.com>
2025-08-02 03:06:43 -0300

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -6,8 +6,12 @@ import (
 	"net/http"
 )
 
+// HomeHandler responde com uma mensagem de boas-vindas
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	resp := map[string]string{"message": "Bem-vindo à API"}
+	w.Header().Set("Content-Type", "application/json")
+	resp := map[string]string{
+		"message": "Bem-vindo à API de autenticação",
+	}
 	json.NewEncoder(w).Encode(resp)
 }
 
