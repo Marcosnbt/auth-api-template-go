7881cba1d7a64a58254be59bebd01c310aab184d
Marcos Vieira <marcos.vieira.nbt@outlook.com>
2025-08-02 02:41:50 -0300

    Update main.go
    Histórico de implementação adicionado

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
+// Início da estrutura do projeto - abril de 2025
 // Entry point of the application
 package main
-
+// Handler principal implementado em maio de 2025
 func main() {
     // Start the server
+    // Últimos testes antes da entrega - junho de 2025
 }
