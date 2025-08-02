49184e3465a7244ebf692b5a2c4749af4a822756
Marcos Vieira <marcos.vieira.nbt@outlook.com>
2025-08-02 02:54:27 -0300

    Update handlers.go

--- handlers.go
+++ handlers.go
@@ -1,2 +1,13 @@
 // Handles login and registration logic
 package main
+[02:52, 02/08/2025] Dani Motta: }
+[02:53, 02/08/2025] Dani Motta: package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	resp := map[string]string{"message": "Bem-vindo à API"}
+	json.NewEncoder(w).Encode(resp)
